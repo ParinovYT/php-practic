0fb4e3118fd411a5853fb7008d464c36e8be1399
unknown <gamerxokks@gmail.com>
2022-04-22 08:45:57 +0300

    php_oop_mvc_95_3

--- practic_frame/project/views/page/act.php
+++ practic_frame/project/views/page/act.php
@@ -5,14 +5,11 @@
   <title></title>
  </head>
  <body>
-<div>
-    это представление
-    действия act контроллера page
-</div>
+<h1><?= $header ?></h1>
 <ul>
-	<li><?php echo $var1; ?></li>
-	<li><?php echo $var2; ?></li>
-	<li><?php echo $var3; ?></li>
+    <?php foreach ($users as $user): ?>
+        <li><?= $user; ?></li>
+    <?php endforeach; ?>
 </ul>
  </body>
 </html> 
